2a5294018027a92adf7ec97e647d615178f0bfcc
Albin123725 <107454411+Albin123725@users.noreply.github.com>
2025-12-07 21:09:19 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 """
-Aternos Auto-Booster Package
+Aternos 24/7 Server Keeper Package
 """
 
-__version__ = "2.0.0"
-__author__ = "Aternos Booster"
+__version__ = "1.0.0"
+__author__ = "Aternos Keeper"
+__description__ = "Keeps Aternos server running 24/7 by clicking +1 button at 0:59"
